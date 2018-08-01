3c34370b268e320304a35d1a4c0df0431cd35a0c
Hugo Letoret <hugo.letoret@gmail.com>
2018-08-01 21:02:52 +0200

    ajout class test

---
+++ test du source control/test.swift
@@ -0,0 +1,16 @@
+//
+//  test.swift
+//  test du source control
+//
+//  Created by Letoret Hugo on 1/08/18.
+//  Copyright © 2018 Letoret Hugo. All rights reserved.
+//
+
+import Foundation
+
+class test {
+
+func salut() {
+    print("salut les codabeilles")
+}
+}
